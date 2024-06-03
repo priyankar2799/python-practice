32e316ea9564bb7511583735f15708d0d9719bb0
priyankar2799 <128132877+priyankar2799@users.noreply.github.com>
2024-06-03 06:54:15 +0000

    update

---
+++ global_local_variables.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+def add():
+    c = 4
+    print(a + b + c)
+
+def sub():
+    print (a - b)   
+
+place = "bangalore"
+age = 26
+
+def student_detail_1():
+    name ="priyanka"
+    print(name + place + str(age))
+    
+def student_detail_2():
+    name="farhan"
+    print(name + place + str(age))
+student_detail_1()
+student_detail_2()
+add()
+sub() 
